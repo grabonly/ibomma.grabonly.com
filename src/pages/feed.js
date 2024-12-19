5f8f4924f57ece8aedafcdb10c1bd731f212bb45
grabonly.com <grabonly.com@gmail.com>
2024-12-19 15:14:50 +0000

    rss feed times updated
    Signed-off-by: grabonly.com <grabonly.com@gmail.com>

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -1,46 +1,78 @@
 import {title} from "../API/defaultSiteValues";
-import { YYYY } from "../API/time";
+import { loadMovies } from "../API/latestMovies";
+import { seoURL } from "../API/seoURL";
+import { YYYY, rssFeedTime } from "../API/time";
+// const rssUpdateTime = rssFeedTime(true)
+// const rssPubTime = rssFeedTime(false);
 const year = YYYY();
+const [resp1, resp2, resp3] = await loadMovies(1);
 
 
 export const GET = ({request}) =>{
     const url = new URL(request.url);
     const rootDomain = url.origin;
     let rssFeed = `<?xml version="1.0" encoding="UTF-8"?>
-    <rss
-      version="2.0"
-      xmlns:content="http://purl.org/rss/1.0/modules/content/"
-      xmlns:wfw="http://wellformedweb.org/CommentAPI/"
-      xmlns:dc="http://purl.org/dc/elements/1.1/"
-      xmlns:atom="http://www.w3.org/2005/Atom"
-      xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
-      xmlns:slash="http://purl.org/rss/1.0/modules/slash/">
-    
+    <rss version="2.0"
+        xmlns:content="http://purl.org/rss/1.0/modules/content/"
+        xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+        xmlns:dc="http://purl.org/dc/elements/1.1/"
+        xmlns:atom="http://www.w3.org/2005/Atom"
+        xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+        xmlns:slash="http://purl.org/rss/1.0/modules/slash/">
+
     <channel>
 
-          <title>${title}</title>
-          <atom:link href="${rootDomain+"/feed/"}" rel="self" type="application/rss+xml" />
-          <link>${rootDomain}</link>
-          <description>${title} ac ${year} - New Telugu, Tamil, Hindi &amp; English Movies HD Quality</description>
-          <lastBuildDate>Wed, 18 Dec 2024 17:45:43 +0000</lastBuildDate>
-          <language>en-US</language>
-          <sy:updatePeriod>hourly</sy:updatePeriod>
-          <sy:updateFrequency>1</sy:updateFrequency>`;
+        <title>${title}</title>
+        <atom:link href="${rootDomain+"/feed/"}" rel="self" type="application/rss+xml" />
+        <link>${rootDomain}</link>
+        <description>${title} ac ${year} - New Telugu, Tamil, Hindi &amp; English Movies HD Quality</description>
+        <lastBuildDate>${rssFeedTime(true)}</lastBuildDate>
+        <language>en-US</language>
+        <sy:updatePeriod>hourly</sy:updatePeriod>
+        <sy:updateFrequency>1</sy:updateFrequency>`;
 
-     rssFeed += `
-
+resp1.results.map(movie =>{
+    rssFeed += `
+    
         <item>
-            <title>How to Learn JavaScript in 30 Days</title>
-            <link>https://myawesomeblog.com/learn-javascript</link>
-            <description>A comprehensive guide to mastering JavaScript quickly.</description>
-            <pubDate>Sun, 17 Dec 2024 09:00:00 +0000</pubDate>
-            <guid>https://myawesomeblog.com/learn-javascript</guid>
+            <title>${movie.title} full movie review</title>
+            <link>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</link>
+            <description>${movie.overview}</description>
+            <pubDate>${rssFeedTime(false)}</pubDate>
+            <guid>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</guid>
         </item>`;
+});
+
+resp2.results.map(movie =>{
+    rssFeed += `
+    
+        <item>
+            <title>${movie.title} full movie review</title>
+            <link>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</link>
+            <description>${movie.overview}</description>
+            <pubDate>${rssFeedTime(false)}</pubDate>
+            <guid>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</guid>
+        </item>`;
+});
+
+resp3.results.map(movie =>{
+    rssFeed += `
+    
+        <item>
+            <title>${movie.title} full movie review</title>
+            <link>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</link>
+            <description>${movie.overview}</description>
+            <pubDate>${rssFeedTime(false)}</pubDate>
+            <guid>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</guid>
+        </item>`;
+});
 
     rssFeed += `
+
     </channel>
 </rss>`
 
+// console.log(resp1)
     return new Response(rssFeed, {
         headers: {
           'Content-Type': 'application/rss+xml',
