a7dbfd3082682cea8c38dc3b43ff81fc44620e4d
grabonly.com <grabonly.com@gmail.com>
2024-12-20 03:30:48 +0000

    feed test
    Signed-off-by: grabonly.com <grabonly.com@gmail.com>

---
+++ src/own/analytics.js
@@ -0,0 +1,10 @@
+// <!-- Google tag (gtag.js) -->
+{/* <script async src=""></script> */}
+
+import "https://www.googletagmanager.com/gtag/js?id=G-JWMJX3XWT0";
+  
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-JWMJX3XWT0');
