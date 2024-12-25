40e5891db7af0a2dd7ca52a6b6fce840a42f16fd
grabonly.com <grabonly.com@gmail.com>
2024-12-25 07:15:22 +0000

    analytics changed
    Signed-off-by: grabonly.com <grabonly.com@gmail.com>

--- src/own/analytics.js
+++ src/own/analytics.js
@@ -1,10 +1,10 @@
-// <!-- Google tag (gtag.js) -->
-{/* <script async src=""></script> */}
+// // <!-- Google tag (gtag.js) -->
+// {/* <script async src=""></script> */}
 
-import "https://www.googletagmanager.com/gtag/js?id=G-JWMJX3XWT0";
+// import "https://www.googletagmanager.com/gtag/js?id=G-JWMJX3XWT0";
   
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
 
-  gtag('config', 'G-JWMJX3XWT0');
+//   gtag('config', 'G-JWMJX3XWT0');
