2242bd8dd5b906e6cf9facfee8231b4734bb903d
grabonly.com <grabonly.com@gmail.com>
2024-12-25 10:45:05 +0000

    [WIP]: ads show problem
    Signed-off-by: grabonly.com <grabonly.com@gmail.com>

--- public/analytics.js
+++
@@ -1,8 +0,0 @@
-{/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-JWMJX3XWT0"></script> */}
-import "https://www.googletagmanager.com/gtag/js?id=G-JWMJX3XWT0";
-
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-
-gtag('config', 'G-JWMJX3XWT0');
