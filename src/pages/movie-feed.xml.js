ff7540be3666c86d77ce6e4657fbd943d63c5157
grabonly.com <grabonly.com@gmail.com>
2024-12-25 07:26:09 +0000

    /movie-feed.xml added
    Signed-off-by: grabonly.com <grabonly.com@gmail.com>

---
+++ src/pages/movie-feed.xml.js
@@ -0,0 +1,82 @@
+import {title} from "../API/defaultSiteValues";
+import { encodeRSSDescription } from "../API/helpers";
+import { moviesList } from "../API/latestMovies";
+import { seoURL } from "../API/seoURL";
+import { YYYY, rssFeedTime } from "../API/time";
+
+
+export const GET = async ({request}) =>{
+    const year = YYYY();
+    const [resp1, resp2, resp3] = await moviesList();
+
+    const url = new URL(request.url);
+    const rootDomain = url.origin;
+
+    let rssFeed = `<?xml version="1.0" encoding="UTF-8"?>
+    <rss version="2.0"
+        xmlns:content="http://purl.org/rss/1.0/modules/content/"
+        xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+        xmlns:dc="http://purl.org/dc/elements/1.1/"
+        xmlns:atom="http://www.w3.org/2005/Atom"
+        xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+        xmlns:slash="http://purl.org/rss/1.0/modules/slash/">
+
+    <channel>
+
+        <title>${title}</title>
+        <atom:link href="${rootDomain+"/feed/"}" rel="self" type="application/rss+xml" />
+        <link>${rootDomain}</link>
+        <description>${title} ac  - ${year} New Telugu, Tamil, Hindi &amp; English Movies HD Quality</description>
+        <lastBuildDate>${rssFeedTime(true)}</lastBuildDate>
+        <language>en-US</language>
+        <sy:updatePeriod>hourly</sy:updatePeriod>
+        <sy:updateFrequency>1</sy:updateFrequency>`;
+
+resp1.results.map(movie =>{
+    rssFeed += `
+    
+        <item>
+            <title>${movie.title} full movie review</title>
+            <link>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</link>
+            <description>${encodeRSSDescription(movie.overview)}</description>
+            <pubDate>${rssFeedTime(false)}</pubDate>
+            <guid>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</guid>
+        </item>`;
+});
+
+resp2.results.map(movie =>{
+    rssFeed += `
+    
+        <item>
+            <title>${movie.title} full movie review</title>
+            <link>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</link>
+            <description>${encodeRSSDescription(movie.overview)}</description>
+            <pubDate>${rssFeedTime(false)}</pubDate>
+            <guid>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</guid>
+        </item>`;
+});
+
+resp3.results.map(movie =>{
+    rssFeed += `
+    
+        <item>
+            <title>${movie.title} full movie review</title>
+            <link>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</link>
+            <description>${encodeRSSDescription(movie.overview)}</description>
+            <pubDate>${rssFeedTime(false)}</pubDate>
+            <guid>${rootDomain+"/movie/"+seoURL(movie.title)+"_"+movie.id+"/"}</guid>
+        </item>`;
+});
+
+
+    rssFeed += `
+
+    </channel>
+</rss>`
+
+    return new Response(rssFeed, {
+        headers: {
+          'Content-Type': 'application/rss+xml',
+        },
+      });
+}
